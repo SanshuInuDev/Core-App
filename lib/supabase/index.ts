909460956ffa0b5ff66c7148f25338346802107f
dev0614 <shimamuratakehiko44@gmail.com>
2024-07-10 08:55:06 +0900

    added supabase env variants in .env

--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dnlbkxckhwbefpzsfhnt.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRubGJreGNraHdiZWZwenNmaG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NTUzNDksImV4cCI6MjAzNDAzMTM0OX0.vagPT52vZDV66TnNmFrPiJnBiOe9txYt4Ueqm269M1o'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? 'https://dnlbkxckhwbefpzsfhnt.supabase.co'
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_API_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRubGJreGNraHdiZWZwenNmaG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NTUzNDksImV4cCI6MjAzNDAzMTM0OX0.vagPT52vZDV66TnNmFrPiJnBiOe9txYt4Ueqm269M1o'
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
